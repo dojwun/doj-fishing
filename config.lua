b2e4ecbd9a8ef16c3df74a40639a66a331aeab36
/dojwun <79622405+dojwun@users.noreply.github.com>
2021-11-02 01:06:56 -0700

    added more locations

--- config.lua
+++ config.lua
@@ -7,7 +7,7 @@ Config.StopFishing =  73                                        -- Stop Fishing 
 Config.BoatPrice = 100                                          -- Price of the boat rental, price is divided when boat is returned
 Config.PedDistance = 60.0                                       -- Distance of ped spawning
 Config.PearlsExoticCard = "pearlscard"                          -- Name of item to sell exotic/illegal fish
-Config.TestFish = false						-- Test Fish with commands /startfish & /spawnfish
+Config.TestFish = false											-- Test Fish with commands /startfish or /spawnfish
 
 Config.fishingRodPrice = 50                                     -- Price of a fishing rod
 Config.fishingBaitPrice = 5                                     -- Price of fishing bait
@@ -18,11 +18,20 @@ Config.sharktigerPrice   = 200                                  -- Price of Tige
 Config.sharkhammerPrice  = 200                                  -- Price of Hammerhead Sharks
 Config.killerwhalePrice  = 500                                  -- Price of Killer whales
 Config.PearlsSellsBlip = vector3(-1816.406, -1193.334, 13.305)  -- Blip for Fish Sells
-Config.ReturnLocation = {
-    coords = vector4(-802.883, -1495.654, 1.595, 294.265),      -- Position a player will spawn at once they return the boat
-}
+
 Config.SpawnLocation = {
-    coords = vector4(-802.05, -1504.36, -0.47, 112.27),         -- Spawn location of fishing boat once its rented
+    LaPuerta	= vector4(-802.05, -1504.36, -0.47, 112.27),        
+	PaletoCove 	= vector4(-1599.932, 5259.831, 1.578, 20.6317),        
+	ElGordo 	= vector4(3372.705, 5188.673, 0.121, 272.03),        
+	ActDam 		= vector4(1701.506, 43.897, 161.767, 188.579),
+	AlamoSea 	= vector4(1294.363, 4232.013, 31.709, 160.441),        
+}
+Config.ReturnLocation = {
+    LaPuerta 	= vector4(-802.883, -1495.654, 1.595, 294.265),      
+	PaletoCove 	= vector4(-1605.342, 5258.052, 2.083, 292.725),       
+	ElGordo 	= vector4(3371.141, 5183.996, 1.46, 99.634),     
+	ActDam 		= vector4(1695.805, 43.456, 161.767, 103.579),
+	AlamoSea 	= vector4(1301.1, 4233.696, 33.909, 337.548),        
 }
 Config.PedList = {                                              -- Peds that will be spawned in
 	{
@@ -31,8 +40,34 @@ Config.PedList = {                                              -- Peds that wil
 		heading = 100.0,
 		gender = "male",
         scenario = "WORLD_HUMAN_STAND_FISHING"
-		-- animDict = "",
-		-- animName = "",
+	},
+	{
+		model = "u_m_m_filmdirector",                            -- Boat/Gear Menu
+		coords = vector3(-1604.236, 5256.483, 1.073),               
+		heading = 291.202,
+		gender = "male",
+        scenario = "WORLD_HUMAN_STAND_FISHING"
+	},
+	{
+		model = "s_m_o_busker_01",                            -- Boat/Gear Menu
+		coords = vector3(3373.215, 5183.515, 0.46),               
+		heading = 266.111,
+		gender = "male",
+        scenario = "WORLD_HUMAN_STAND_FISHING"
+	},
+	{
+		model = "ig_cletus",                            -- Boat/Gear Menu
+		coords = vector3(1694.811, 39.927, 160.767),               
+		heading = 191.786,
+		gender = "male",
+        scenario = "WORLD_HUMAN_STAND_FISHING"
+	},
+	{
+		model = "a_m_m_hillbilly_01",                            -- Boat/Gear Menu
+		coords = vector3(1299.665, 4231.885, 32.909),               
+		heading = 81.693,
+		gender = "male",
+        scenario = "WORLD_HUMAN_STAND_FISHING"
 	},
     {
 		model = "s_m_y_busboy_01",
