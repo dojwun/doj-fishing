eec0c5b0a3f018a1f093628ce5447ff9454704ef
/dojwun <79622405+dojwun@users.noreply.github.com>
2021-11-02 13:54:46 -0700

    grammer fix

--- config.lua
+++ config.lua
@@ -20,14 +20,14 @@ Config.sharkhammerPrice  = 200                                  -- Price of Hamm
 Config.killerwhalePrice  = 500                                  -- Price of Killer whales
 Config.PearlsSellsBlip = vector3(-1816.406, -1193.334, 13.305)  -- Blip for Fish Sells
 
-Config.SpawnLocation = {
+Config.BoatSpawnLocation = {
     LaPuerta	= vector4(-802.05, -1504.36, -0.47, 112.27),        
 	PaletoCove 	= vector4(-1599.932, 5259.831, 1.578, 20.6317),        
 	ElGordo 	= vector4(3372.705, 5188.673, 0.121, 272.03),        
 	ActDam 		= vector4(1701.506, 43.897, 161.767, 188.579),
 	AlamoSea 	= vector4(1294.363, 4232.013, 31.709, 160.441),        
 }
-Config.ReturnLocation = {
+Config.PlayerReturnLocation = {
     LaPuerta 	= vector4(-802.883, -1495.654, 1.595, 294.265),      
 	PaletoCove 	= vector4(-1605.342, 5258.052, 2.083, 292.725),       
 	ElGordo 	= vector4(3371.141, 5183.996, 1.46, 99.634),     
