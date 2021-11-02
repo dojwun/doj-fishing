aba22c4e2c4c475b3b90a1b507d8d2923210d764
/dojwun <79622405+dojwun@users.noreply.github.com>
2021-11-02 01:06:00 -0700

    init

---
+++ client/addons.lua
@@ -0,0 +1,472 @@
+function BoatMenuLaPuerta()
+    exports['qb-menu']:openMenu({
+        {
+            header = "Fishing Boat Rental",
+            isMenuHeader = true
+        },
+        {
+            header = "Boat: "..Config.RentalBoat,
+            txt = "Price: $"..Config.BoatPrice,
+            params = {
+                event = "doj:client:rentaBoat",
+				args = 1
+            }
+        },
+        {
+            header = "Close",
+            txt = "",
+            params = {
+                event = "qb-menu:closeMenu"
+            }
+        },
+    })
+end 
+
+function BoatMenuPaletoCove()
+    exports['qb-menu']:openMenu({
+        {
+            header = "Fishing Boat Rental",
+            isMenuHeader = true
+        },
+        {
+            header = "Boat: "..Config.RentalBoat,
+            txt = "Price: $"..Config.BoatPrice,
+            params = {
+                event = "doj:client:rentaBoat",
+				args = 2
+            }
+        },
+        {
+            header = "Close",
+            txt = "",
+            params = {
+                event = "qb-menu:closeMenu"
+            }
+        },
+    })
+end 
+
+function BoatMenuElGordo()
+    exports['qb-menu']:openMenu({
+        {
+            header = "Fishing Boat Rental",
+            isMenuHeader = true
+        },
+        {
+            header = "Boat: "..Config.RentalBoat,
+            txt = "Price: $"..Config.BoatPrice,
+            params = {
+                event = "doj:client:rentaBoat",
+				args = 3
+            }
+        },
+        {
+            header = "Close",
+            txt = "",
+            params = {
+                event = "qb-menu:closeMenu"
+            }
+        },
+    })
+end 
+
+
+function BoatMenuActDam()
+    exports['qb-menu']:openMenu({
+        {
+            header = "Fishing Boat Rental",
+            isMenuHeader = true
+        },
+        {
+            header = "Boat: "..Config.RentalBoat,
+            txt = "Price: $"..Config.BoatPrice,
+            params = {
+                event = "doj:client:rentaBoat",
+				args = 4
+            }
+        },
+        {
+            header = "Close",
+            txt = "",
+            params = {
+                event = "qb-menu:closeMenu"
+            }
+        },
+    })
+end 
+
+function BoatMenuAlamoSea()
+    exports['qb-menu']:openMenu({
+        {
+            header = "Fishing Boat Rental",
+            isMenuHeader = true
+        },
+        {
+            header = "Boat: "..Config.RentalBoat,
+            txt = "Price: $"..Config.BoatPrice,
+            params = {
+                event = "doj:client:rentaBoat",
+				args = 5
+            }
+        },
+        {
+            header = "Close",
+            txt = "",
+            params = {
+                event = "qb-menu:closeMenu"
+            }
+        },
+    })
+end 
+--============================================================== ReturnMenus
+
+function ReturnBoatLaPuerta()
+    exports['qb-menu']:openMenu({
+		{
+            header = "Fishing Boat Rental",
+            isMenuHeader = true
+        },
+		{
+            header = "Return Boat",
+            txt = "return and get $"..math.floor(Config.BoatPrice/2),
+            params = {
+                event = "doj:client:ReturnBoat",
+				args = 1
+            }
+        },
+        {
+            header = "Close",
+            txt = "",
+            params = {
+                event = "qb-menu:closeMenu"
+            }
+        },
+    })
+end 
+
+function ReturnBoatPaletoCove()
+    exports['qb-menu']:openMenu({
+		{
+            header = "Fishing Boat Rental",
+            isMenuHeader = true
+        },
+		{
+            header = "Return Boat",
+            txt = "return and get $"..math.floor(Config.BoatPrice/2),
+            params = {
+                event = "doj:client:ReturnBoat",
+				args = 2
+            }
+        },
+        {
+            header = "Close",
+            txt = "",
+            params = {
+                event = "qb-menu:closeMenu"
+            }
+        },
+    })
+end 
+
+function ReturnBoatElGordo()
+    exports['qb-menu']:openMenu({
+		{
+            header = "Fishing Boat Rental",
+            isMenuHeader = true
+        },
+		{
+            header = "Return Boat",
+            txt = "return and get $"..math.floor(Config.BoatPrice/2),
+            params = {
+                event = "doj:client:ReturnBoat",
+				args = 3
+            }
+        },
+        {
+            header = "Close",
+            txt = "",
+            params = {
+                event = "qb-menu:closeMenu"
+            }
+        },
+    })
+end 
+
+function ReturnBoatActDam()
+    exports['qb-menu']:openMenu({
+		{
+            header = "Fishing Boat Rental",
+            isMenuHeader = true
+        },
+		{
+            header = "Return Boat",
+            txt = "return and get $"..math.floor(Config.BoatPrice/2),
+            params = {
+                event = "doj:client:ReturnBoat",
+				args = 4
+            }
+        },
+        {
+            header = "Close",
+            txt = "",
+            params = {
+                event = "qb-menu:closeMenu"
+            }
+        },
+    })
+end 
+
+function ReturnBoatAlamoSea()
+    exports['qb-menu']:openMenu({
+		{
+            header = "Fishing Boat Rental",
+            isMenuHeader = true
+        },
+		{
+            header = "Return Boat",
+            txt = "return and get $"..math.floor(Config.BoatPrice/2),
+            params = {
+                event = "doj:client:ReturnBoat",
+				args = 5
+            }
+        },
+        {
+            header = "Close",
+            txt = "",
+            params = {
+                event = "qb-menu:closeMenu"
+            }
+        },
+    })
+end
+
+--============================================================== Sell/Gear Menus
+
+RegisterNetEvent('doj:client:SellLegalFish')
+AddEventHandler('doj:client:SellLegalFish', function()
+    exports['qb-menu']:openMenu({
+		{
+            header = "Pearl's Seafood Restaurant",
+            isMenuHeader = true
+        },
+		{
+            header = "Sell Fish",
+            txt = "Current Price: $"..Config.fishPrice.." each",
+            params = {
+				isServer = true,
+                event = "fishing:server:SellLegalFish",
+				args = 1
+            }
+        },
+		{
+            header = "Sell Stingrays",
+            txt = "Current Price: $"..Config.stingrayPrice.." each",
+            params = {
+				isServer = true,
+                event = "fishing:server:SellLegalFish",
+				args = 2
+            }
+        },
+        {
+            header = "Close",
+            txt = "",
+            params = {
+                event = "qb-menu:closeMenu"
+            }
+        },
+    })
+end)
+
+RegisterNetEvent('doj:client:buyFishingGear')
+AddEventHandler('doj:client:buyFishingGear', function() 
+    exports['qb-menu']:openMenu({
+		{
+            header = "Purchase Fishing Gear",
+            isMenuHeader = true
+        },
+		{
+            header = "Buy Fishing Rod",
+            txt = "$"..Config.fishingRodPrice,
+            params = {
+				isServer = true,
+                event = "fishing:server:BuyFishingGear",
+				args = 1
+            }
+        },
+		{
+            header = "Buy Fishing Bait",
+            txt = "$"..Config.fishingBaitPrice,
+            params = {
+				isServer = true,
+                event = "fishing:server:BuyFishingGear",
+				args = 2
+            }
+        },
+        {
+            header = "Close",
+            txt = "",
+            params = {
+                event = "qb-menu:closeMenu"
+            }
+        },
+    })
+end)
+
+RegisterNetEvent('doj:client:SellillegalFish')
+AddEventHandler('doj:client:SellillegalFish', function() 
+	QBCore.Functions.TriggerCallback('QBCore:HasItem', function(HasItem)
+		if HasItem then
+			local charinfo = QBCore.Functions.GetPlayerData().charinfo
+			QBCore.Functions.Notify('Welcome, '..charinfo.firstname..' '..charinfo.lastname)
+			exports['qb-menu']:openMenu({
+				{
+					header = "Pearl's Seafood Restaurant",
+					isMenuHeader = true
+				},
+				{
+					header = "Sell Dolphin",
+					txt = "Current Price: $"..Config.dolphinPrice.." each",
+					params = {
+						isServer = true,
+						event = "fishing:server:SellillegalFish",
+						args = 1
+					}
+				},
+				{
+					header = "Sell Tiger Shark",
+					txt = "Current Price: $"..Config.sharktigerPrice.." each",
+					params = {
+						isServer = true,
+						event = "fishing:server:SellillegalFish",
+						args = 2
+					}
+				},
+				{
+					header = "Sell Hammerhead Shark",
+					txt = "Current Price: $"..Config.sharkhammerPrice.." each",
+					params = {
+						isServer = true,
+						event = "fishing:server:SellillegalFish",
+						args = 3
+					}
+				},
+				{
+					header = "Sell Killer Whale",
+					txt = "Current Price: $"..Config.killerwhalePrice.." each",
+					params = {
+						isServer = true,
+						event = "fishing:server:SellillegalFish",
+						args = 4
+					}
+				},
+				{
+					header = "Close",
+					txt = "",
+					params = {
+						event = "qb-menu:closeMenu"
+					}
+				},
+			})
+		else
+			QBCore.Functions.Notify('You cannot sell to us at the moment, sorry for the inconvenience', 'error', 3500)
+		end
+	end, Config.PearlsExoticCard)
+end)
+
+--============================================================== Exports
+
+exports['qb-target']:AddTargetModel(`s_m_y_ammucity_01`, {
+    options = {
+        {
+            event = "doj:BoatMenuLaPuerta",
+            icon = "fas fa-anchor",
+            label = "Boat Menu",
+        },
+		{
+            event = "doj:client:buyFishingGear",
+            icon = "fas fa-fish",
+            label = "Buy Fishing Gear",
+        },
+    },
+    distance = 10.0
+})
+
+exports['qb-target']:AddTargetModel(`u_m_m_filmdirector`, {
+    options = {
+        {
+            event = "doj:BoatMenuPaletoCove",
+            icon = "fas fa-anchor",
+            label = "Boat Menu",
+        },
+		{
+            event = "doj:client:buyFishingGear",
+            icon = "fas fa-fish",
+            label = "Buy Fishing Gear",
+        },
+    },
+    distance = 10.0
+})
+
+exports['qb-target']:AddTargetModel(`s_m_o_busker_01`, {
+    options = {
+        {
+            event = "doj:BoatMenuElGordo",
+            icon = "fas fa-anchor",
+            label = "Boat Menu",
+        },
+		{
+            event = "doj:client:buyFishingGear",
+            icon = "fas fa-fish",
+            label = "Buy Fishing Gear",
+        },
+    },
+    distance = 10.0
+})
+
+exports['qb-target']:AddTargetModel(`ig_cletus`, {
+    options = {
+        {
+            event = "doj:BoatMenuActDam",
+            icon = "fas fa-anchor",
+            label = "Boat Menu",
+        },
+		{
+            event = "doj:client:buyFishingGear",
+            icon = "fas fa-fish",
+            label = "Buy Fishing Gear",
+        },
+    },
+    distance = 10.0
+})
+
+exports['qb-target']:AddTargetModel(`a_m_m_hillbilly_01`, {
+    options = {
+        {
+            event = "doj:BoatMenuAlamoSea",
+            icon = "fas fa-anchor",
+            label = "Boat Menu",
+        },
+		{
+            event = "doj:client:buyFishingGear",
+            icon = "fas fa-fish",
+            label = "Buy Fishing Gear",
+        },
+    },
+    distance = 10.0
+})
+
+exports['qb-target']:AddTargetModel(`s_m_y_busboy_01`, {
+    options = {
+        {
+            event = "doj:client:SellLegalFish",
+            icon = "fa fa-fish",
+            label = "Sell Fish",
+        },
+		{
+            event = "doj:client:SellillegalFish",
+            icon = "fa fa-fish",
+            label = "Sell Exotic Fish",
+        },
+    },
+    distance = 10.0
+}) 
