d753589d2887de0edfa950466fb7e7b0c8da38a5
/dojwun <79622405+dojwun@users.noreply.github.com>
2021-11-02 01:04:48 -0700

    added more locations

--- client/client.lua
+++ client/client.lua
@@ -42,30 +42,6 @@ Citizen.CreateThread(function()
 end)
 
 Citizen.CreateThread(function()
-	Blip = AddBlipForCoord(Config.ReturnLocation.coords)
-	SetBlipSprite(Blip, 410)
-	SetBlipDisplay(Blip, 4)
-	SetBlipScale(Blip, 0.8)
-	SetBlipColour(Blip, 74)
-    SetBlipAsShortRange(Blip, true)
-    BeginTextCommandSetBlipName("STRING")
-    AddTextComponentSubstringPlayerName("La-Puerta Fishing")
-    EndTextCommandSetBlipName(Blip)
-end)
-
-Citizen.CreateThread(function()
-	Blip = AddBlipForCoord(Config.PearlsSellsBlip)
-	SetBlipSprite(Blip, 68)
-	SetBlipDisplay(Blip, 4)
-	SetBlipScale(Blip, 0.8)
-	SetBlipColour(Blip, 74)
-    SetBlipAsShortRange(Blip, true)
-    BeginTextCommandSetBlipName("STRING")
-    AddTextComponentSubstringPlayerName("Pearl's Seafood Fish Sells")
-    EndTextCommandSetBlipName(Blip)
-end)
-
-Citizen.CreateThread(function()
 	while true do
 		Citizen.Wait(1)
 		if fishing then
@@ -185,8 +161,6 @@ AddEventHandler('fishing:SkillBar', function(message)
     end
 end)
 
-
-
 RegisterNetEvent('fishing:break')
 AddEventHandler('fishing:break', function()
 	fishing = false
@@ -297,22 +271,140 @@ AddEventHandler('fishing:fishstart', function()
 	end
 end, false)
 
-RegisterNetEvent('doj:OpenBoatsMenu')
-AddEventHandler('doj:OpenBoatsMenu', function()
+RegisterNetEvent('doj:client:ReturnBoat')
+AddEventHandler('doj:client:ReturnBoat', function(args)
+	local ped = PlayerPedId()
+	local args = tonumber(args)
+	if args == 1 then 
+		if IsPedInAnyVehicle(ped) then
+			local boat = GetVehiclePedIsIn(ped,true) 
+			QBCore.Functions.DeleteVehicle(boat)
+			SetEntityCoords(ped, Config.ReturnLocation.LaPuerta.x, Config.ReturnLocation.LaPuerta.y, Config.ReturnLocation.LaPuerta.z, 0, 0, 0, false) 
+			SetEntityHeading(ped, Config.ReturnLocation.LaPuerta.w)
+			TriggerServerEvent('fishing:server:returnDeposit')
+		end
+	elseif args == 2 then
+		if IsPedInAnyVehicle(ped) then
+			local boat = GetVehiclePedIsIn(ped,true) 
+			QBCore.Functions.DeleteVehicle(boat)
+			SetEntityCoords(ped, Config.ReturnLocation.PaletoCove.x, Config.ReturnLocation.PaletoCove.y, Config.ReturnLocation.PaletoCove.z, 0, 0, 0, false) 
+			SetEntityHeading(ped, Config.ReturnLocation.PaletoCove.w)
+			TriggerServerEvent('fishing:server:returnDeposit')
+		end
+	elseif args == 3 then
+		if IsPedInAnyVehicle(ped) then
+			local boat = GetVehiclePedIsIn(ped,true) 
+			QBCore.Functions.DeleteVehicle(boat)
+			SetEntityCoords(ped, Config.ReturnLocation.ElGordo.x, Config.ReturnLocation.ElGordo.y, Config.ReturnLocation.ElGordo.z, 0, 0, 0, false) 
+			SetEntityHeading(ped, Config.ReturnLocation.ElGordo.w)
+			TriggerServerEvent('fishing:server:returnDeposit')
+		end
+	elseif args == 3 then
+		if IsPedInAnyVehicle(ped) then
+			local boat = GetVehiclePedIsIn(ped,true) 
+			QBCore.Functions.DeleteVehicle(boat)
+			SetEntityCoords(ped, Config.ReturnLocation.ActDam.x, Config.ReturnLocation.ActDam.y, Config.ReturnLocation.ActDam.z, 0, 0, 0, false) 
+			SetEntityHeading(ped, Config.ReturnLocation.ActDam.w)
+			TriggerServerEvent('fishing:server:returnDeposit')
+		end
+	else
+		if IsPedInAnyVehicle(ped) then
+			local boat = GetVehiclePedIsIn(ped,true) 
+			QBCore.Functions.DeleteVehicle(boat)
+			SetEntityCoords(ped, Config.ReturnLocation.AlamoSea.x, Config.ReturnLocation.AlamoSea.y, Config.ReturnLocation.AlamoSea.z, 0, 0, 0, false) 
+			SetEntityHeading(ped, Config.ReturnLocation.AlamoSea.w)
+			TriggerServerEvent('fishing:server:returnDeposit')
+		end
+	end
+end)
+
+RegisterNetEvent('doj:client:rentaBoat')
+AddEventHandler('doj:client:rentaBoat', function(args)
+	local args = tonumber(args)
+	if args == 1 then 
+		QBCore.Functions.TriggerCallback('fishing:server:checkMoney', function(isSuccess)
+			if isSuccess then
+				QBCore.Functions.SpawnVehicle(Config.RentalBoat, function(boat)
+					SetVehicleNumberPlateText(boat, "Rent-a-Boat")
+					exports['LegacyFuel']:SetFuel(boat, 100.0)
+					SetEntityHeading(boat, Config.SpawnLocation.LaPuerta.w)
+					TaskWarpPedIntoVehicle(PlayerPedId(), boat, -1)
+					TriggerEvent("vehiclekeys:client:SetOwner", GetVehicleNumberPlateText(boat))
+					SetVehicleEngineOn(boat, true, true)
+				end, Config.SpawnLocation.LaPuerta, true) 
+			end
+		end)
+    elseif args == 2 then 
+		QBCore.Functions.TriggerCallback('fishing:server:checkMoney', function(isSuccess)
+			if isSuccess then
+				QBCore.Functions.SpawnVehicle(Config.RentalBoat, function(boat)
+					SetVehicleNumberPlateText(boat, "Rent-a-Boat")
+					exports['LegacyFuel']:SetFuel(boat, 100.0)
+					SetEntityHeading(boat, Config.SpawnLocation.PaletoCove.w)
+					TaskWarpPedIntoVehicle(PlayerPedId(), boat, -1)
+					TriggerEvent("vehiclekeys:client:SetOwner", GetVehicleNumberPlateText(boat))
+					SetVehicleEngineOn(boat, true, true)
+				end, Config.SpawnLocation.PaletoCove, true) 
+			end
+		end)
+	elseif args == 3 then 
+		QBCore.Functions.TriggerCallback('fishing:server:checkMoney', function(isSuccess)
+			if isSuccess then
+				QBCore.Functions.SpawnVehicle(Config.RentalBoat, function(boat)
+					SetVehicleNumberPlateText(boat, "Rent-a-Boat")
+					exports['LegacyFuel']:SetFuel(boat, 100.0)
+					SetEntityHeading(boat, Config.SpawnLocation.ElGordo.w)
+					TaskWarpPedIntoVehicle(PlayerPedId(), boat, -1)
+					TriggerEvent("vehiclekeys:client:SetOwner", GetVehicleNumberPlateText(boat))
+					SetVehicleEngineOn(boat, true, true)
+				end, Config.SpawnLocation.ElGordo, true) 
+			end
+		end)
+	elseif args == 3 then
+		QBCore.Functions.TriggerCallback('fishing:server:checkMoney', function(isSuccess)
+			if isSuccess then
+				QBCore.Functions.SpawnVehicle(Config.RentalBoat, function(boat)
+					SetVehicleNumberPlateText(boat, "Rent-a-Boat")
+					exports['LegacyFuel']:SetFuel(boat, 100.0)
+					SetEntityHeading(boat, Config.SpawnLocation.ActDam.w)
+					TaskWarpPedIntoVehicle(PlayerPedId(), boat, -1)
+					TriggerEvent("vehiclekeys:client:SetOwner", GetVehicleNumberPlateText(boat))
+					SetVehicleEngineOn(boat, true, true)
+				end, Config.SpawnLocation.ActDam, true) 
+			end
+		end)
+	else
+		QBCore.Functions.TriggerCallback('fishing:server:checkMoney', function(isSuccess)
+			if isSuccess then
+				QBCore.Functions.SpawnVehicle(Config.RentalBoat, function(boat)
+					SetVehicleNumberPlateText(boat, "Rent-a-Boat")
+					exports['LegacyFuel']:SetFuel(boat, 100.0)
+					SetEntityHeading(boat, Config.SpawnLocation.AlamoSea.w)
+					TaskWarpPedIntoVehicle(PlayerPedId(), boat, -1)
+					TriggerEvent("vehiclekeys:client:SetOwner", GetVehicleNumberPlateText(boat))
+					SetVehicleEngineOn(boat, true, true)
+				end, Config.SpawnLocation.AlamoSea, true) 
+			end
+		end)
+    end  
+end)
+
+RegisterNetEvent('doj:BoatMenuLaPuerta')
+AddEventHandler('doj:BoatMenuLaPuerta', function()
 	local ped = PlayerPedId()
     local inVehicle = IsPedInAnyVehicle(ped)
 	if Config.Fish24hours then
 		if inVehicle then
-			ReturnBoatMenu()
+			ReturnBoatLaPuerta()
 		else
-			OpenBoatMenu()
+			BoatMenuLaPuerta()
 		end
 	else
 		if GetClockHours() >= 6 and GetClockHours() <= 21 then
 			if inVehicle then
-				ReturnBoatMenu()
+				ReturnBoatLaPuerta()
 			else
-				OpenBoatMenu()
+				BoatMenuLaPuerta()
 			end
 		else
 			QBCore.Functions.Notify('Too Late to Fish!, Opened from 6:00am / 21:00pm', 'error')
@@ -320,163 +412,96 @@ AddEventHandler('doj:OpenBoatsMenu', function()
 	end
 end)
 
-RegisterNetEvent('doj:client:ReturnBoat')
-AddEventHandler('doj:client:ReturnBoat', function()
+RegisterNetEvent('doj:BoatMenuPaletoCove')
+AddEventHandler('doj:BoatMenuPaletoCove', function()
 	local ped = PlayerPedId()
-	if IsPedInAnyVehicle(ped) then
-		local boat = GetVehiclePedIsIn(ped,true) 
-		QBCore.Functions.DeleteVehicle(boat)
-		SetEntityCoords(ped, Config.ReturnLocation.coords.x, Config.ReturnLocation.coords.y, Config.ReturnLocation.coords.z, 0, 0, 0, false) 
-		SetEntityHeading(ped, Config.ReturnLocation.coords.w)
-		TriggerServerEvent('fishing:server:returnDeposit')
+    local inVehicle = IsPedInAnyVehicle(ped)
+	if Config.Fish24hours then
+		if inVehicle then
+			ReturnBoatPaletoCove()
+		else
+			BoatMenuPaletoCove()
+		end
+	else
+		if GetClockHours() >= 6 and GetClockHours() <= 21 then
+			if inVehicle then
+				ReturnBoatPaletoCove()
+			else
+				BoatMenuPaletoCove()
+			end
+		else
+			QBCore.Functions.Notify('Too Late to Fish!, Opened from 6:00am / 21:00pm', 'error')
+		end
 	end
 end)
 
-RegisterNetEvent('doj:client:rentaBoat')
-AddEventHandler('doj:client:rentaBoat', function()
-	QBCore.Functions.TriggerCallback('fishing:server:checkMoney', function(isSuccess)
-        if isSuccess then
-			QBCore.Functions.SpawnVehicle(Config.RentalBoat, function(boat)
-				SetVehicleNumberPlateText(boat, "Rent-a-Boat")
-				exports['LegacyFuel']:SetFuel(boat, 100.0)
-				SetEntityHeading(boat, Config.SpawnLocation.coords.w)
-				TaskWarpPedIntoVehicle(PlayerPedId(), boat, -1)
-				TriggerEvent("vehiclekeys:client:SetOwner", GetVehicleNumberPlateText(boat))
-				SetVehicleEngineOn(boat, true, true)
-			end, Config.SpawnLocation.coords, true) 
-        end
-    end)       
-end)
-
-RegisterNetEvent('doj:client:SellLegalFish')
-AddEventHandler('doj:client:SellLegalFish', function()
-    exports['qb-menu']:openMenu({
-		{
-            header = "Pearl's Seafood Restaurant",
-            isMenuHeader = true
-        },
-		{
-            header = "Sell Fish",
-            txt = "Current Price: $"..Config.fishPrice.." each",
-            params = {
-				isServer = true,
-                event = "fishing:server:SellLegalFish",
-				args = 1
-            }
-        },
-		{
-            header = "Sell Stingrays",
-            txt = "Current Price: $"..Config.stingrayPrice.." each",
-            params = {
-				isServer = true,
-                event = "fishing:server:SellLegalFish",
-				args = 2
-            }
-        },
-        {
-            header = "Close",
-            txt = "",
-            params = {
-                event = "qb-menu:closeMenu"
-            }
-        },
-    })
-end)
-
-RegisterNetEvent('doj:client:buyFishingGear')
-AddEventHandler('doj:client:buyFishingGear', function() 
-    exports['qb-menu']:openMenu({
-		{
-            header = "Purchase Fishing Gear",
-            isMenuHeader = true
-        },
-		{
-            header = "Buy Fishing Rod",
-            txt = "$"..Config.fishingRodPrice,
-            params = {
-				isServer = true,
-                event = "fishing:server:BuyFishingGear",
-				args = 1
-            }
-        },
-		{
-            header = "Buy Fishing Bait",
-            txt = "$"..Config.fishingBaitPrice,
-            params = {
-				isServer = true,
-                event = "fishing:server:BuyFishingGear",
-				args = 2
-            }
-        },
-        {
-            header = "Close",
-            txt = "",
-            params = {
-                event = "qb-menu:closeMenu"
-            }
-        },
-    })
-end)
-
-RegisterNetEvent('doj:client:SellillegalFish')
-AddEventHandler('doj:client:SellillegalFish', function() 
-	QBCore.Functions.TriggerCallback('QBCore:HasItem', function(HasItem)
-		if HasItem then
-			local charinfo = QBCore.Functions.GetPlayerData().charinfo
-			QBCore.Functions.Notify('Welcome, '..charinfo.firstname..' '..charinfo.lastname)
-			exports['qb-menu']:openMenu({
-				{
-					header = "Pearl's Seafood Restaurant",
-					isMenuHeader = true
-				},
-				{
-					header = "Sell Dolphin",
-					txt = "Current Price: $"..Config.dolphinPrice.." each",
-					params = {
-						isServer = true,
-						event = "fishing:server:SellillegalFish",
-						args = 1
-					}
-				},
-				{
-					header = "Sell Tiger Shark",
-					txt = "Current Price: $"..Config.sharktigerPrice.." each",
-					params = {
-						isServer = true,
-						event = "fishing:server:SellillegalFish",
-						args = 2
-					}
-				},
-				{
-					header = "Sell Hammerhead Shark",
-					txt = "Current Price: $"..Config.sharkhammerPrice.." each",
-					params = {
-						isServer = true,
-						event = "fishing:server:SellillegalFish",
-						args = 3
-					}
-				},
-				{
-					header = "Sell Killer Whale",
-					txt = "Current Price: $"..Config.killerwhalePrice.." each",
-					params = {
-						isServer = true,
-						event = "fishing:server:SellillegalFish",
-						args = 4
-					}
-				},
-				{
-					header = "Close",
-					txt = "",
-					params = {
-						event = "qb-menu:closeMenu"
-					}
-				},
-			})
+RegisterNetEvent('doj:BoatMenuElGordo')
+AddEventHandler('doj:BoatMenuElGordo', function()
+	local ped = PlayerPedId()
+    local inVehicle = IsPedInAnyVehicle(ped)
+	if Config.Fish24hours then
+		if inVehicle then
+			ReturnBoatElGordo()
 		else
-			QBCore.Functions.Notify('You cannot sell to us at the moment, sorry for the inconvenience', 'error', 3500)
+			BoatMenuElGordo()
 		end
-	end, Config.PearlsExoticCard)
+	else
+		if GetClockHours() >= 6 and GetClockHours() <= 21 then
+			if inVehicle then
+				ReturnBoatElGordo()
+			else
+				BoatMenuElGordo()
+			end
+		else
+			QBCore.Functions.Notify('Too Late to Fish!, Opened from 6:00am / 21:00pm', 'error')
+		end
+	end
+end)
+
+RegisterNetEvent('doj:BoatMenuActDam')
+AddEventHandler('doj:BoatMenuActDam', function()
+	local ped = PlayerPedId()
+    local inVehicle = IsPedInAnyVehicle(ped)
+	if Config.Fish24hours then
+		if inVehicle then
+			ReturnBoatActDam()
+		else
+			BoatMenuActDam()
+		end
+	else
+		if GetClockHours() >= 6 and GetClockHours() <= 21 then
+			if inVehicle then
+				ReturnBoatActDam()
+			else
+				BoatMenuActDam()
+			end
+		else
+			QBCore.Functions.Notify('Too Late to Fish!, Opened from 6:00am / 21:00pm', 'error')
+		end
+	end
+end)
+
+RegisterNetEvent('doj:BoatMenuAlamoSea')
+AddEventHandler('doj:BoatMenuAlamoSea', function()
+	local ped = PlayerPedId()
+    local inVehicle = IsPedInAnyVehicle(ped)
+	if Config.Fish24hours then
+		if inVehicle then
+			ReturnBoatAlamoSea()
+		else
+			BoatMenuAlamoSea()
+		end
+	else
+		if GetClockHours() >= 6 and GetClockHours() <= 21 then
+			if inVehicle then
+				ReturnBoatAlamoSea()
+			else
+				BoatMenuAlamoSea()
+			end
+		else
+			QBCore.Functions.Notify('Too Late to Fish!, Opened from 6:00am / 21:00pm', 'error')
+		end
+	end
 end)
 
 --============================================================== Functions
@@ -487,52 +512,6 @@ function RequestTheModel(model)
 		Citizen.Wait(0)
 	end
 end
-
-function OpenBoatMenu()
-    exports['qb-menu']:openMenu({
-        {
-            header = "Fishing Boat Rental",
-            isMenuHeader = true
-        },
-        {
-            header = "Boat: "..Config.RentalBoat,
-            txt = "Price: $"..Config.BoatPrice,
-            params = {
-                event = "doj:client:rentaBoat",
-            }
-        },
-        {
-            header = "Close",
-            txt = "",
-            params = {
-                event = "qb-menu:closeMenu"
-            }
-        },
-    })
-end 
-
-function ReturnBoatMenu()
-    exports['qb-menu']:openMenu({
-		{
-            header = "Fishing Boat Rental",
-            isMenuHeader = true
-        },
-		{
-            header = "Return Boat",
-            txt = "return and get $"..math.floor(Config.BoatPrice/2),
-            params = {
-                event = "doj:client:ReturnBoat",
-            }
-        },
-        {
-            header = "Close",
-            txt = "",
-            params = {
-                event = "qb-menu:closeMenu"
-            }
-        },
-    })
-end 
 
 function nearPed(model, coords, heading, gender, animDict, animName, scenario)
 	RequestModel(GetHashKey(model))
@@ -562,7 +541,7 @@ function nearPed(model, coords, heading, gender, animDict, animName, scenario)
 		TaskPlayAnim(ped, animDict, animName, 8.0, 0, -1, 1, 0, 0, 0)
 	end
 	if scenario then
-		TaskStartScenarioInPlace(ped, scenario, 0, true)
+		TaskStartScenarioInPlace(ped, scenario, 0, true) 
 	end
 	for i = 0, 255, 51 do
 		Citizen.Wait(50)
@@ -571,38 +550,3 @@ function nearPed(model, coords, heading, gender, animDict, animName, scenario)
 	
 	return ped
 end
-
---============================================================== Exports
-
-exports['qb-target']:AddTargetModel(`s_m_y_ammucity_01`, {
-    options = {
-        {
-            event = "doj:OpenBoatsMenu",
-            icon = "fas fa-anchor",
-            label = "Boat Menu",
-        },
-		{
-            event = "doj:client:buyFishingGear",
-            icon = "fas fa-fish",
-            label = "Buy Fishing Gear",
-        },
-    },
-    distance = 10.0
-}) 
-
-exports['qb-target']:AddTargetModel(`s_m_y_busboy_01`, {
-    options = {
-        {
-            event = "doj:client:SellLegalFish",
-            icon = "fa fa-fish",
-            label = "Sell Fish",
-        },
-		{
-            event = "doj:client:SellillegalFish",
-            icon = "fa fa-fish",
-            label = "Sell Exotic Fish",
-        },
-    },
-    distance = 10.0
-}) 
-
